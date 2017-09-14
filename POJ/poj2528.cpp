3b810bbe5dbae2f0fe3d70678af9f7462cc36d6a
fanese <525039107@qq.com>
2017-09-14 22:11:33 +0800

    poj2558 a segement tree practice

--- POJ/poj2528.cpp
+++ POJ/poj2528.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 #define lson l , m ,rt << 1
 #define rson m + 1 , r , rt << 1 | 1
-const int maxn=20000+10;
+const int maxn=40000+10;
 struct Poster
 {
 	int lend,rend;
@@ -20,46 +20,46 @@ struct Poster
 Poster a[maxn],b[maxn];
 typedef pair<int,int> pii;
 vector<pii> G;
-int n,tree[maxn << 2],lazy[maxn << 2];
-
-void PushUp(int rt)
+int n,tree[maxn << 2];
+bool exist[maxn];
+void Pushdown(int rt)
 {
-	if(tree[ rt << 1 ] == tree[ rt << 1 | 1])
-		tree[rt] = tree[ rt << 1 ];
-	else
-		tree[rt]=-1;
-}
-
-
-void build(int l , int r, int rt)
-{
-	if(l == r)
-	{
-		tree[rt] = 0;
+	if(tree[rt]==0)
 		return ;
-	}
-	build(lson);
-	build(rson);
-	PushUp(rt);
+	tree[rt << 1] = tree[rt << 1 | 1] = tree[rt];
+	tree[rt]=0;
 }
 
 void update(int val , int L , int R , int l , int r , int rt)
 {
-	if(L <= l && R >= r && tree[rt] == 0)
+	if(L <= l && R >= r)
 	{
-		lazy[rt] = val;
 		tree[rt] = val;
 		return ;
 	}
+	int m = (l + r) >> 1;
+	Pushdown(rt);
+	if(L <= m)
+		update(val,L,R,lson);
+	if(R > m)
+		update(val,L,R,rson);
 
 }
-
-int cmp(pii X , pii Y)
+void query(int l,int r,int rt)
 {
-	if(X.first!=Y.first)
-		return X.first < Y.first;
-
+	if(tree[rt])
+	{
+		exist[tree[rt]]=true;
+		return ;
+	}
+	int m = (l + r) >> 1;
+	if(l == r)
+		return ;
+	query(lson);
+	query(rson);
 }
+
+
 
 int main(void)
 {
@@ -76,31 +76,45 @@ int main(void)
 			G.push_back(make_pair( aa , i << 1 ));
 			G.push_back(make_pair( bb , i << 1 | 1 ));
 		}
-		sort(G.begin(),G.end(),cmp);
+		sort(G.begin(),G.end());
 		vector<pii>::iterator it;
+		int N = G.size();
+		for(int i = 1; i < N; i++)
+		{
+			if(G[i].first - G[i-1].first > 1)
+				G.push_back(make_pair(G[i-1].first+1,-1));
+		}
+		sort(G.begin(),G.end());
 		int pre=-1,cnt=0;
-		bool flag=false;
 		for( it = G.begin() ; it != G.end() ; it++)
 		{
 			int val = it->first, idx = it->second;
-			if(val == pre)
-			{
-				if(flag)
-					cnt++;
-			}
 			if(val != pre)
 			{
 				pre = val;
 				cnt++;
 			}
+			if(idx == -1)
+				continue;
 			if(idx & 1)
-				b[it - G.begin()].rend = cnt;
+				b[idx >> 1].rend = cnt;
 			else
-				b[it - G.begin()].lend = cnt;
+				b[idx >> 1].lend = cnt;
 			
 		}
-		build(1 , maxn , 1);
-
+		memset(tree,0,sizeof(tree));
+		for(int i = 0; i < n; i++)
+		{
+			update(i + 1,b[i].lend,b[i].rend,1,maxn,1);
+		}
+		int ans = 0;
+		memset(exist,0,sizeof(exist));
+		query(1,maxn,1);
+		for(int i = 1; i <= n; i++)
+		{
+			ans += exist[i];
+		}
+		printf("%d\n",ans);
 	}
 	return 0;
 }
